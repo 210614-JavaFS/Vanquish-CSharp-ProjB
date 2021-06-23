62e6a0ca79316350efebaca788412ac8864b5a96
DannyLGZ <linggezeng@gmail.com>
2021-06-23 17:16:34 -0400

    updated

--- src/fixtures/rooms/DinningRoom.java
+++ src/fixtures/rooms/DinningRoom.java
@@ -7,20 +7,16 @@ public class DinningRoom extends fixtures.rooms.Room{
 		super("Dinning Room",
 				"A classic formal dinning room",
 				"The room has a darken curtain and elegant furnitures, chandelier lighting, china cabinet for storage of fine "
-				+ "pieces and dishes. A note on a rectangular table.");
+				+ "pieces and dishes. A note on a rectangular table."+\n
+				+ "Take the north exit to Kitchen or west exit to Yoga Room.");
 	}
-
-//	@Override
-//	public void interactwith() {
-//	
-//	}
 	
 	
 	//For the use of RoomManager.java 
 	
 //	DinningRoom dinningroom = new DinningRoom();
 //	this.rooms[7] = dinningroom;
-//	dinningroom.setExit("kitchen",null,null,null);
+//	dinningroom.setExit("kitchen",null,null,yogaroom);
 //	Curtain dinningCurtain = new Curtain();
 //	Note dinningNote = new Note();
 //	dinningroom.setMethod(dinningCurtain,dinningNote);
