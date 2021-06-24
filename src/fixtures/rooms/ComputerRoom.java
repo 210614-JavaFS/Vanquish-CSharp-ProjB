33298feb4f3f608495fa0f9a3c236d6f44335e83
Munibk22 <Munib22@gmail.com>
2021-06-24 10:21:21 -0400

    added  room and set exit in the COmputer room for review

--- src/fixtures/rooms/ComputerRoom.java
+++ src/fixtures/rooms/ComputerRoom.java
@@ -1,9 +1,6 @@
 package fixtures.rooms;
-import fixtures.object.Curtain;
+
 import fixtures.object.Interactive;
-import fixtures.object.Lamp;
-import fixtures.object.Note;
-import fixtures.object.YogaMat;
 
 public class ComputerRoom extends fixtures.rooms.Room implements Interactive{
 	ComputerRoom[] exits;
