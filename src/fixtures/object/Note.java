ac6cb0a1642a4b734be409a5184efd5edeb3ecc6
DannyLGZ <linggezeng@gmail.com>
2021-06-24 14:01:59 -0400

    updated

--- src/fixtures/object/Note.java
+++ src/fixtures/object/Note.java
@@ -8,13 +8,13 @@ public class Note extends Fixture implements Interactive{
 		super();
 		this.name = "Note";
 		this.shortDescription = "A note on the rectangular table.";
-		this.longDescription = "There is a not left on the dinning table, seems someone left a message to you.";
+		this.longDescription = "There is a not left on the dinning table, "
+				+ "seems someone left a message to you.";
 	}
-
 	@Override
 	public void interactwith() {
 		System.out.println("You pick up the note and read it.");
-		this.longDescription = "The message on the note, 'The training is gonna be tough and stress, stay sharp.'";
-	}
-	
+		this.longDescription = "The message on the note, "
+				+ "'The training is gonna be tough and stress, stay sharp.'";
+	}	
 }
