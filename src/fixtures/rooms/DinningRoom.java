3554600ebd8dd608fd04696cd098186f51c6b638
DannyLGZ <linggezeng@gmail.com>
2021-06-24 18:37:06 -0400

    updated

--- src/fixtures/rooms/DinningRoom.java
+++ src/fixtures/rooms/DinningRoom.java
@@ -1,20 +1,13 @@
 package fixtures.rooms;
 
-public class DinningRoom extends fixtures.rooms.Room{
-	
+public class DinningRoom extends fixtures.rooms.Room{	
 	//The room is located on the south of kitchen.
 	public DinningRoom() {
-		super("Dinning Room",
-				"A classic formal dinning room",
+		super("Dining Room",
+				"A classic formal dining room",
 				"The room has a darken curtain and elegant furnitures, chandelier lighting, china cabinet for storage of fine "
 				+ "pieces and dishes. A note on a rectangular table."+ "\n"
-				+ "Take the north exit to bedroom chamber or west exit to Yoga Room.");
-	}
-
-	@Override
-	public void interactwith() {
-		// TODO Auto-generated method stub
-		
+				+ "Take the north exit to Bedroom or west exit to Yoga Room.");
 	}
 	
 	
