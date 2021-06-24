e921c78efabafd2c69e8cf27746b189eec17a8d4
RaquelCode <rqlmoura@yahoo.com>
2021-06-23 22:38:27 -0700

    New object added

---
+++ src/fixtures/object/Door.java
@@ -0,0 +1,21 @@
+package fixtures.object;
+
+import fixtures.Fixture;
+
+public class Door extends Fixture implements Interactive {
+
+	public Door() {
+		super();
+		this.name = "Door";
+		this.shortDescription = "A closed door is on the bathroom";
+		this.longDescription = "The closed door seems to be locked. The bathroom is currently occupied.";
+
+	}
+
+	@Override
+	public void interactwith() {
+		System.out.print("You still have to wait until you see the unoccupied sign in the door.");
+		this.longDescription = "The door of the bathroom is open. Now you can use the bathroom.";
+	}
+
+}
