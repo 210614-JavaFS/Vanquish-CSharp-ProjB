6b39f3eb1c2f960c200816c8c561f3f852c207eb
Munibk22 <Munib22@gmail.com>
2021-06-24 17:39:26 -0400

    stashing

--- src/fixtures/object/Computer.java
+++ src/fixtures/object/Computer.java
@@ -1,36 +1,65 @@
 package fixtures.object;
 
+import java.util.Scanner;
+
 import fixtures.Fixture;
 
 public class Computer extends Fixture implements Interactive{
-	
+	Scanner  scan= new Scanner(System.in);
 	public boolean powerOn;
 	public String model;
+	public String[] games = new String[4];
+	public String choice;
 	
 	public Computer() {
 		super();
+		this.games[0]= "CounterStrike";
+		this.games[1]= "Call of Duty";
+		this.games[2]= "Zork";
+		this.games[3]= "spring tool suite";
 		this.powerOn = false;
 		this.name = "Computer";
-		this.shortDescription = "Turn on the computer and it to house blueprints.";
-		this.longDescription = "A computer for publc use. User can print house blueprints. \n"
-				+  "The computer is currently turned on?. \n" + powerOn;
-			
+		this.shortDescription = "Turn on the computer and do programming or play games.";
+		this.longDescription = "A computer for publc use. User can do programming or play games. \n"
+				+  "The computer is currently turned on?. \n" + this.powerOn;			
 		this.model = "AMD Ryzen 9 5900X";
-	}
-		
-	public void interactWith() {
-		System.out.println("You have switched on the computer");
-		powerOn=true;
-		if(powerOn) {
-			this.longDescription = "A computer for publc use. User can print house blueprints.  \n"
-					+  "The computer is currently turned on. \n" + powerOn;
-		}
+	
+	}	
 
-	}
 
 	@Override
-	public void interactwith() {
-		// TODO Auto-generated method stub
+	public void interactwith() {	
+		this.powerOn=true;
+		
+		if(this.powerOn) {
+			System.out.println("You have switched on the computer");
+			this.longDescription = "A computer for public use. User can do programming or play games.  \n"
+					+  "The computer is currently turned off. \n" + powerOn;
+			
+		}
+		this.longDescription = "Games " 
+				+  "The computer is currently turned on. \n" + powerOn + " \n" + this.model + " \n"
+		+ "Launching " + this.games[3];
+		
 		
 	}
+	
+	public String playGame() {
+		String instructor = "Instructor reaction : :-)";
+		
+		System.out.println("Choose your game" + games[0]+ games[1]+ games[2] );
+		String answer = scan.nextLine();
+		scan.close();
+		return instructor + " \n" + answer;
+		
+	}
+	
+	public String doProgramming() {
+		String instructor = "Instructor reaction : :-)";
+		String coding = "Starting " + this.games[3];
+		return instructor + " \n" + coding;
+		
+	}
+
+	
 }
--- src/fixtures/object/Printer.java
+++ src/fixtures/object/Printer.java
@@ -2,39 +2,34 @@ package fixtures.object;
 
 import fixtures.Fixture;
 
-public class Printer extends Fixture implements Interactive {	
-
-	public boolean powerOn;
-	public int numPages = 100;
+public class Printer extends Fixture implements Interactive {
 	
+	public boolean powerOn;
+	public int numPages;
+		
 	public Printer() {
 		super();
-		this.name = "Printer";
-		this.shortDescription = "Print house blueprints.";
-		this.longDescription = "Print house blueprints.";
-	}
-	
-	
-	public Printer(boolean powerOn, int numPages) {
-		super();
-		this.powerOn = powerOn;
-		this.numPages = numPages;
+		this.powerOn = false;
+		this.numPages = 100;
 		this.name = "Printer";
 		this.shortDescription = "Print house blueprints.";
 		this.longDescription = "A Printer for publc use. User can print house blueprints. \n"
-				+  "The printer is currently turned on? " + powerOn + "\n"
-			+ "The printer has " + numPages + " pages left in the tray.";
+				+  "The printer is currently turned on?. " + this.powerOn  + " \n"
+			+ "The printer has " + this.numPages + " pages left in the tray.";
 	}
 
 	@Override
 	public void interactwith() {
 		powerOn = true;
-		if (powerOn) {
-			numPages = numPages - 5;
+		
+		if (this.powerOn) {
+			this.numPages = this.numPages - 5;
 			System.out.println("Printing house blue prints \n"
-			+ "The printer has " + numPages + " pages left in the tray.");
+			+ "The printer has " + this.numPages + " pages left in the tray.");
 		}
-	
+		this.longDescription = "The printer is currently turned on?. \n" + this.powerOn + " \n" 
+				+ "Printing castle blueprints. \n"
+				+ "The printer has " + this.numPages + " pages left in the tray.";
 		
 	}
 	
